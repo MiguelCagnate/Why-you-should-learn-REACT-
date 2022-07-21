b84a97bfc013f32a4d43f055ff030caae94427a4
MiguelCagnate <omikeflies@gmail.com>
2022-07-21 12:43:02 +0200

    nurvo

---
+++ src/MainContent.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function MainContent() {
+  return (
+    <div className="MainContent">
+      <h1>Reasons I'm excited to learn React</h1>
+      <ul>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+        <li>React native is built by Facebook and they have made it MIT license, that means it is open source</li> 
+        <li>React Has a Large Development Community</li>
+        <li>React is a JavaScript library</li>
+        <li>JSX increases the performance and efficiency of ReactJS</li>
+        <li>React Offers Reusable Components</li>
+
+      </ul>
+    </div>
+  );
+}
